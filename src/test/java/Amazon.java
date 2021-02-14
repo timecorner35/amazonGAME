74644c6d9982865a557c7ba43c39fb807109ea12
aga <adamimdj@gmail.com>
2021-02-14 22:18:54 +0000

    initial commit

--- src/test/java/Amazon.java
+++ src/test/java/Amazon.java
@@ -21,15 +21,15 @@ public class Amazon {
         for (WebElement coin : coins) {
             group.add(coin.getText());
         }
-        int numberOfWeighs=1;
-        while (group.size() >= 3){
+        int numberOfWeighings = 1;
+        while (group.size() >= 3) {
             List<String> group1 = new ArrayList<>();
             List<String> group2 = new ArrayList<>();
             List<String> group3 = new ArrayList<>();
             for (int i = 0; i < group.size() / 3; i++) {
                 group1.add(group.get(i));
-                group2.add(group.get((group.size()/3+i)));
-                group3.add(group.get((group.size()/3*2+i)));
+                group2.add(group.get((group.size() / 3 + i)));
+                group3.add(group.get((group.size() / 3 * 2 + i)));
             }
             for (int i = 0; i < group.size() / 3; i++) {
                 driver.findElement(By.id("left_" + i + "")).sendKeys("" + group1.get(i));
@@ -52,14 +52,15 @@ public class Amazon {
             }
             if (group.size() == 1) {
                 coins.get(Integer.parseInt(group.get(0))).click();
-                System.out.println("numberOfWeighs = " + numberOfWeighs);
                 Thread.sleep(1000);
                 Alert alert = driver.switchTo().alert();
                 System.out.println(alert.getText());
                 alert.accept();
+                System.out.println(driver.findElement(By.className("game-info")).getText());
+                System.out.println("numberOfWeighings = " + numberOfWeighings);
                 driver.quit();
             }
-            numberOfWeighs++;
+            numberOfWeighings++;
         }
     }
 }
